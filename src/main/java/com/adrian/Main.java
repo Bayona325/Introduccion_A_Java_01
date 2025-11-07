422403911bdcce477075d6c2718ab5306952acd1
Adrian Bayona <adrianbayona325@gmail.com>
2025-11-07 11:10:37 -0500

    feat: :zap: exercies

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -1,5 +1,9 @@
 package com.adrian;
 
+// import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String cadena = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.";
@@ -21,5 +25,33 @@ public class Main {
         // Static - Para operaciones y no almacenan valores
         Condicionales.diaSemana(3);
 
+    }
+
+    final static int maxRange = 10000;
+    public static void main1(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Ingrese el valor minimo: ");
+        var min = scan.nextInt();
+        System.out.println("Ingrese el valor maximo: ");
+        var max = scan.nextInt();
+
+
+        int[] numbers = new int[maxRange];
+
+        for(int a=0; a < numbers.length; a++) {
+            numbers[a] = random.nextInt(min, max+1);
+        }
+
+        System.out.println("Ingrese la cantidad de numeros a buscar: ");
+        var counter = scan.nextInt();
+
+        for(int a=0; a < counter; a++) {
+            System.out.println("Ingrese el inidice a buscar [1 hasta"+maxRange+"]: ");
+            var index = scan.nextInt();
+            System.out.println("Oiga el indice es: "+ (index)+" = " + numbers[index - 1]);
+        }
+        scan.close();
     }
 }
