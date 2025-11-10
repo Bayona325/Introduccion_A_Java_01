d49d7738a72a0c573260dd5750a2d00fe8a6288d
Adrian Bayona <adrianbayona325@gmail.com>
2025-11-10 10:36:37 -0500

    feat: :zap: Exercises

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -13,20 +13,30 @@ public class Main {
         var biblioteka = new Bibliotek();
         while (flag) {
             System.out.println("""
-                    ++++++++++++++ MENU ++++++++++++++
-                      ** Ingrese la opción deseada **
-                    1. Listar libros Biblioteca
-                    2. Guardar libros Biblioteca
+                    ++++++++++++++  MENU  ++++++++++++++
+                            ** Ingrese la opcion deseada **
+                    1. Listar libros Bibliotek
+                    2. Guardar libros Bibliotek
                     0. Salir
                     """);
-            switch (args) {
-                case value:
-                    
+            switch (scan.nextInt()) {
+                case 1:
+                    biblioteka.listarLibros();
                     break;
-            
+                case 2:
+                    biblioteka.guardar(scan);
+                    break;
+                case 0:
+                    // Salir
+                    flag = false;
+
+                    break;
                 default:
+                    System.out.println("Ingrese una opcion valida......\nPresione Enter para continuar.");
+                    scan.nextLine();
                     break;
             }
         }
+        scan.close();
     }
 }
--- src/main/java/com/adrian/ejercicios/Bibliotek.java
+++ src/main/java/com/adrian/ejercicios/Bibliotek.java
@@ -17,27 +17,29 @@ public class Bibliotek {
         }
     }
 
-    public void guardar() {
+    public void guardar(Scanner scanner) {
         if(indice >= 10) {
-            System.out.println("No hay más espacio en la bibliotek.....");
+            System.out.println("No hay mas espacio en la Bibliotek.....");
             return;
         }
-        
-        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine(); // Evitar el salto de linea
         Libro libro = new Libro();
         System.out.println("Ingresa el título del libro: ");
-        libro.titulo = scanner.nextLine();
+        libro.titulo = scanner.nextLine(); 
         System.out.println("Ingresa el autor del libro: ");
-        libro.autor = scanner.nextLine();
-        System.out.println("Ingresa el año de publicación: ");
-        libro.anio = scanner.nextInt();
+        libro.autor = scanner.nextLine(); 
+        System.out.println("Ingresa el año de publicación:  ");
+        libro.anio = scanner.nextInt(); 
         scanner.nextLine(); // Evitar el salto de linea
-        System.out.println("Ingresa el género del libro: ");
-        libro.genero = scanner.nextLine();
-        System.out.println("Ingresa el número de páginas: ");
+        System.out.println("Ingresa el género del libro:  ");
+        libro.genero = scanner.nextLine(); 
+        System.out.println("Ingresa el número de páginas:  ");
         libro.numPaginas = scanner.nextInt();
-
-        libros[indice++] = libro;
-        scanner.close();
+        
+        libros[indice++] = libro; //0
+        
+        //libros[indice] = libro;
+        //indice++;
+        //scanner.close();
     }
 }
