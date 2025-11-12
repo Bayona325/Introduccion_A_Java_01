6fb0b7ff99c65a1fb4963673346b0c140f6f5707
Adrian Bayona <adrianbayona325@gmail.com>
2025-11-12 12:45:34 -0500

    feat: :zap: exercices

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -4,91 +4,34 @@ package com.adrian;
 // import java.util.Random;
 import java.util.Scanner;
 
-import com.adrian.ejercicios.Bibliotek;
-import com.adrian.ejercicios.Camper;
-import com.adrian.ejercicios.Factorial;
-import com.adrian.ejercicios.Trainer;
-import com.adrian.game.ahoracado.Ahorcado;
+import com.adrian.ejercicios.bank.BankAccount;
 
 public class Main {
+    final static int maxRange = 10000;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
-        var biblioteka = new Bibliotek();
-        var ahoracado = new Ahorcado();
-        var factorial = new Factorial();
+        var account = new BankAccount();
         while (flag) {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
                             ** Ingrese la opcion deseada **
-                    1. Listar libros Bibliotek
-                    2. Guardar libros Bibliotek
-                    3. Jugar al Ahorcado
-                    4. Vueltos
-                    5. Camper
-                    6. Trainer
+                    1. Consignar
+                    2. Retirar
+                    3. Ver Saldo
                     0. Salir
                     """);
             switch (scan.nextInt()) {
                 case 1:
-                    biblioteka.listarLibros();
+                    System.out.println("Ingrese el monto en numeros enteros a depositar: ");
+                    account.deposit(scan.nextInt());
                     break;
                 case 2:
-                    biblioteka.guardar(scan);
+                    System.out.println("Ingrese el monto en numeros enteros a retirar: ");
+                    account.withdraw(scan.nextInt());
                     break;
                 case 3:
-                    ahoracado.iniciar();
-                    ahoracado.ejecutar(scan);
-                    break;
-                case 4:
-                    System.out.println(factorial.fact(5));
-                    System.out.println(factorial.vueltos(10550, 50));
-                    break;
-                case 5:
-                    //Administrativo
-                    //Trainer
-                    //Colaborador
-                    Camper camper = new Camper("Andres", "Perea", 16, "TI", "1005123456", "?", "andresitto_777@gmail.com", "018000918080");
-                    System.out.println(camper.telefono);
-                    /*camper.nombre = "Andres";
-                    camper.apellido = "Perea";
-                    camper.edad = 16;
-                    camper.tipoDocumento = "TI";
-                    camper.documento = "1005123456";
-                    camper.estratoSocial = -1;
-                    camper.genero = "?";
-                    camper.email = "andresitto_777@gmail.com";*/
-                    camper.telefono = "018000918080";
-                    System.out.println(camper.telefono);
-                    //camper.nivelIngles = 10;
-
-                    camper.dormir();
-                    camper.divertirse();
-
-                    camper.reportar();
-                    camper.calificar();
-
-                    break;
-                case 6:
-                    Trainer trainer = new Trainer();
-                    trainer.nombre = "Adrian";
-                    trainer.apellido = "Triener";
-                    /*
-                    trainer.edad = 27;
-                    trainer.tipoDocumento = "CC";
-                    trainer.documento = "1005198456";
-                    trainer.estratoSocial = 4;
-                    trainer.genero = "Hombre";
-                    trainer.email = "adrian_trainer@gmail.com";
-                    trainer.telefono = "0180009";
-                    trainer.nivelIngles = 10;
-                    */
-
-                    trainer.dormir();
-                    trainer.divertirse();
-
-                    trainer.reportar();
-                    trainer.calificar();
+                    System.out.println("Su Saldo es: $"+account.showBalance());
                     break;
                 case 0:
                     // Salir
