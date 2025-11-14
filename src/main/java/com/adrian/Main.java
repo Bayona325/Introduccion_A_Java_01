97c9b981c059043820e2e0db1956d6499b5bd8f9
Adrian Bayona <adrianbayona325@gmail.com>
2025-11-14 12:41:46 -0500

    feat: :zap: Pagos

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -33,56 +33,16 @@ public class Main {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
                       ** Ingrese la opcion deseada **
-                    1. Ingrese su nombre
-                    2. Ingrese su contraseña
-                    3. Valide su contraseña
-                    4. Agregar permisos (ADMINISTRADORES)
-                    5. Ver permisos (ADMINISTRADORES)
-                    6. Ver info
+                    1. 
                     0. Salir
                     """);
             switch (scan.nextInt()) {
-                case 6:
-                    System.out.println(usuarios.get(0).showInfo());
-                    System.out.println(usuarios.get(1).showInfo());
-                    break;
-                case 5:
-                    for (User item : usuarios) {
-                        if(item instanceof Admin) {
-                            var admin = (Admin)item;
-                            System.out.println(admin.showPermissions());
-                        }
-                    }
-                    break;
-                case 4:
-                    scan.nextLine();
-                    var permiso = scan.nextLine();
-                    var permisos = new String[] { "x", "y", "z" };
-                    // common.setPermissions();
-                    ((Admin)usuarios.get(1)).setPermissions(permiso);
-                    ((Admin) usuarios.get(2)).setPermissions(permisos);
-                    // ((Admin)common).setPermissions(permisos); X
-                    break;
                 case 1:
-                    scan.nextLine();
-                    var tempName = scan.nextLine();
-                    usuarios.get(0).setUsername(tempName);
-                    usuarios.get(1).setUsername(tempName);
-                    usuarios.get(2).setUsername(tempName);
+                    
                     break;
                 case 2:
-                    scan.nextLine();
-                    var tempPassword = scan.nextLine();
-                    usuarios.get(0).setPassword(tempPassword);
-                    usuarios.get(1).setPassword(tempPassword);
-                    usuarios.get(2).setPassword(tempPassword);
                     break;
                 case 3:
-                    scan.nextLine();
-                    var tempCheckPassword = scan.nextLine();
-                    System.out.println("User: " + usuarios.get(0).checkPassword(tempCheckPassword));
-                    System.out.println("Admin: " + usuarios.get(1).checkPassword(tempCheckPassword));
-                    System.out.println("User-Admin: " + usuarios.get(2).checkPassword(tempCheckPassword));
                     break;
                 case 0:
                     // Salir
