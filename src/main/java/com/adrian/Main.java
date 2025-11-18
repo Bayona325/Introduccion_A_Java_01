f79a3f6a8a63aa77530823a191953bb394f6a4be
Adrian Bayona <adrianbayona325@gmail.com>
2025-11-18 10:21:56 -0500

    feat: :zap: Pagos

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -1,48 +1,53 @@
 package com.adrian;
 
-import com.adrian.ejercicios.User.User;
-import com.adrian.ejercicios.User.Admin;
-import com.adrian.ejercicios.User.Cliente;
+import com.adrian.ejercicios.pagos.Pago;
+import com.adrian.ejercicios.pagos.PagoDebito;
+import com.adrian.ejercicios.pagos.PagoEfectivo;
 
-// import java.util.Objects;
-// import java.util.Random;
+import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
     final static int maxRange = 10000;
+
     public static void main(String[] args) {
-        /**
-         * Crea una clase User con los atributos privados username y password.
-         * Implementa los métodos setUsername(String username),
-         * setPassword(String password) y checkPassword(String inputPassword) que compare contraseñas. 
-         */
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
-        // User[] otraforma = new User[100];
-        List<User> usuarios = new ArrayList<>();
 
-        usuarios.add(new Cliente());
-        usuarios.add(new Admin());
-        usuarios.add(new Admin());
-
-        // User common = new Cliente(); // 0
-        // Admin admin = new Admin(); // 1
-        // User user = new Admin(); // 2
-
+        List<Pago> historial = new ArrayList<>();
+    
         while (flag) {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
-                      ** Ingrese la opcion deseada **
-                    1. 
+                            ** Ingrese la opcion deseada **
+                    1. Historial de Pagos
+                    2. Registrar un pago en Efectivo
+                    3. Registrar un pago con Tarjeta Debito
                     0. Salir
                     """);
             switch (scan.nextInt()) {
-                case 1:
-                    
+                case 3:
+                    System.out.println("Ingrese el valor a consignar: ");
+                    var valorDebit = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Ingrese los ultimos 4 digitos: ");
+                    var lasDigits = scan.nextInt() + "";
+                    var debito = new PagoDebito(valorDebit, lasDigits);
+                    historial.add(debito);
                     break;
                 case 2:
+                    System.out.println("Ingrese el valor a consignar: ");
+                    var valorEfectivo = scan.nextInt();
+                    //scan.nextLine();
+                    var efectivo = new PagoEfectivo(valorEfectivo, LocalDate.now(), UUID.randomUUID().toString());
+                    historial.add(efectivo);
                     break;
-                case 3:
+                case 1:
+                    System.out.println("-----------------------HISTORIAL-----------------------");
+                    for (Pago pago : historial) {
+                        System.out.println(pago.informacion());
+                    }
+                    System.out.println("-------------------------FIN--------------------------");
                     break;
                 case 0:
                     // Salir
